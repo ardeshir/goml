7581026d23fcc1f6091bb028f800d8414bcf1358
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-03-05 22:57:45 +0000

    adding label2.go

---
+++ label2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+    "github.com/kniren/gota/dataframe"
+)
+
+func main() {
+	// Open the iris dataset file
+	f, err := os.Open("iris_label2.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+    irisDF := dataframe.ReadCSV(f)
+
+    // Create a filter for the dataframe
+    filter := dataframe.F{
+	Colname: "species",
+	Comparator: "==",
+	Comparando: "Iris-setosa",
+    }
+
+    // Filter the dataframe to see only the rows where 
+    // the iris species is "Iris-versicolor"
+    versicolorDF := irisDF.Filter(filter)
+    if versicolorDF.Err != nil {
+	log.Fatal(versicolorDF.Err)
+    }
+
+	fmt.Println("Gota will format the dataframe\n")
+	fmt.Println(irisDF)
+
+	// filter the dataframe again, but only select out the lab1_width & species columns
+	versicolorDF = irisDF.Filter(filter).Select([]string{"sepalen", "species"})
+	fmt.Println(versicolorDF)
+
+	// filter the dataframe again, but only display
+	// first three results
+    versicolorDF = irisDF.Filter(filter).Select([]string{"sepalen", "species"}).Subset([]int{0,1,2})
+
+	fmt.Println(versicolorDF)
+}
